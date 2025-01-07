0f0b39e08e6735a5a3c985d525f36e3bb4d989e2
yeol0324 <73329772+yeol0324@users.noreply.github.com>
2025-01-08 08:12:43 +0900

    union type , intersection type

--- first.ts
+++ first.ts
@@ -10,7 +10,7 @@ const tuple: [string, number] = ["1", 2];
 // tuple[2]=3 얜 오류!
 tuple.push(3);
 
-// MEMO: enum 자바스크립트로 변환 시 사라짐
+// enum 자바스크립트로 변환 시 사라짐
 const enum EDirection { // 아무값도 주지 않으면 첫번째 숫자부터 순서대로 흘러감
   Up = 5,
   Down, //6
@@ -18,7 +18,7 @@ const enum EDirection { // 아무값도 주지 않으면 첫번째 숫자부터 
   Right, //8
 }
 
-// MEMO: as const자바스크립트로 변환 시 남아있음! 객체로 선언(as const)
+// as const자바스크립트로 변환 시 남아있음! 객체로 선언(as const)
 const ODirection = {
   Up: 0,
   Down: 1,
@@ -37,3 +37,28 @@ run(ODirection.Up);
 
 const obj = { a: 1, b: 2, c: 3 };
 type Key = keyof typeof obj;
+
+// type으로 type 선언
+type A = { a: string; b: number };
+const typea: A = { a: "1", b: 2 };
+
+// interface로 type 선언: 상속,implements 등
+interface B {
+  a: string;
+  b: number;
+}
+const interfaceb: B = { a: "1", b: 2 };
+
+// union | 또는: 사용할 때 신중하자
+// function unionAdd(x: number | string, y: number | string): string | number {
+//   return x + y;
+// }
+// const result: string = add(1,2) 이게 성립 됨
+
+// intersection & 그리고: 주로 객체에서 사용
+type IntersectionA = { hello: "world" } & { name: "yurim" };
+const intersectionA: IntersectionA = {
+  // a: "test", Object literal may only specify known properties, and 'a' does not exist in type 'IntersectionA'.
+  hello: "world",
+  name: "yurim",
+};
